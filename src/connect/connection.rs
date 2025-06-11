f02a5710860d1ec920d0900541e9bfd456b04fb8
Charterino <charterinod@gmail.com>
2025-06-11 12:05:23 +0300

    feat: add connect_and_handshake

---
+++ src/connect/connection.rs
@@ -0,0 +1,76 @@
+use std::ops::DerefMut;
+
+use anyhow::Result;
+use bumpalo::Bump;
+use deadpool::unmanaged::Object;
+use sha2::{Digest, Sha256};
+use tokio::{
+    io::{AsyncWriteExt, BufReader, BufWriter},
+    net::tcp::{OwnedReadHalf, OwnedWriteHalf},
+};
+
+use crate::packets::{
+    magic::ACTIVE_MAGIC,
+    packetheader::{PacketHeader, read_header},
+    packetpayload::{Packet, PacketPayloadType, read_payload},
+    version::Version,
+};
+
+use super::{DESERIALIZE_POOL, SERIALIZE_POOL};
+
+#[derive(Debug)]
+pub struct Connection {
+    pub read_stream: BufReader<OwnedReadHalf>,
+    pub write_stream: BufWriter<OwnedWriteHalf>,
+}
+
+impl Connection {
+    pub async fn write_packet<'a>(&mut self, packet: &'a PacketPayloadType<'a>) -> Result<()> {
+        let mut serialize_buffer = SERIALIZE_POOL.get().await.unwrap();
+        let buf = serialize_buffer.deref_mut();
+        packet.serialize(buf);
+        let mut hash = Sha256::digest(&buf);
+        hash = Sha256::digest(hash);
+        let shorthash = &hash.as_slice()[..4];
+
+        self.write_stream.write_u32_le(ACTIVE_MAGIC).await?;
+        self.write_stream.write_all(packet.command()).await?;
+        self.write_stream.write_u32_le(buf.len() as u32).await?;
+        self.write_stream.write_all(shorthash).await?;
+        self.write_stream.write_all(&buf).await?;
+
+        self.write_stream.flush().await?;
+
+        buf.clear();
+
+        Ok(())
+    }
+
+    pub async fn read_header(&mut self) -> Result<PacketHeader> {
+        read_header(&mut self.read_stream).await
+    }
+
+    pub async fn prepare_for_read(&self) -> Object<Bump> {
+        let mut v = DESERIALIZE_POOL.get().await.unwrap();
+        v.reset();
+        v
+    }
+
+    pub async fn read_packet<'a>(
+        &mut self,
+        header: PacketHeader,
+        allocator: &'a mut Object<Bump>,
+    ) -> Result<Packet<'a>> {
+        let payload = read_payload(&mut self.read_stream, allocator, &header).await?;
+        Ok(Packet {
+            header: header,
+            payload: payload,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct HandshakedConnection<'a> {
+    pub inner: Connection,
+    pub remote_version: Version<'a>,
+}
---
+++ src/connect/mod.rs
@@ -0,0 +1,135 @@
+use std::{
+    borrow::Cow,
+    sync::LazyLock,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+
+use anyhow::{Result, bail};
+use bumpalo::Bump;
+use connection::{Connection, HandshakedConnection};
+use deadpool::unmanaged::Pool;
+use rand::RngCore;
+use tokio::{
+    io::{BufReader, BufWriter},
+    select,
+    time::{self, Instant},
+};
+
+use crate::{
+    packets::{
+        MAX_PACKET_SIZE, deepclone::DeepClone, packetpayload::PacketPayloadType, varstr::VarStr,
+        verack::VerAck, version::Version,
+    },
+    types::addressportnetwork::AddressPortNetwork,
+};
+
+pub mod connection;
+
+const HANDSHAKE_TIMEOUT: Duration = Duration::from_secs(5);
+
+static SERIALIZE_POOL: LazyLock<Pool<Vec<u8>>> = LazyLock::new(|| {
+    let mut pool = Vec::with_capacity(32);
+    for _ in 0..32 {
+        pool.push(Vec::with_capacity(MAX_PACKET_SIZE));
+    }
+    Pool::from(pool)
+});
+
+static DESERIALIZE_POOL: LazyLock<Pool<Bump<1>>> = LazyLock::new(|| {
+    let mut pool = Vec::with_capacity(1024);
+    for _ in 0..1024 {
+        let b = Bump::with_capacity(MAX_PACKET_SIZE);
+        b.set_allocation_limit(Some(MAX_PACKET_SIZE));
+        pool.push(b);
+    }
+    Pool::from(pool)
+});
+
+pub async fn connect_and_handshake(peer: &AddressPortNetwork) -> Result<HandshakedConnection> {
+    let mut conn = connect(peer).await?;
+
+    let deadline = Instant::now().checked_add(HANDSHAKE_TIMEOUT).unwrap();
+
+    select! {
+        _ = time::sleep_until(deadline) => {
+            // Deadline reached and we still havent handshaked! What is this peer...
+            bail!("handshake deadline reached")
+        },
+        handshake_result = handshake(&mut conn) => {
+            match handshake_result {
+                Err(e) => Err(e),
+                Ok(remote_version) => Ok(HandshakedConnection { inner: conn, remote_version: remote_version })
+            }
+        }
+    }
+}
+
+async fn handshake<'a>(conn: &mut Connection) -> Result<Version<'a>> {
+    let mut version = Version::default();
+    version.services = 9;
+    version.timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    version.nonce = rand::rng().next_u64();
+    version.user_agent = VarStr {
+        inner: Cow::Borrowed(b"semikek"),
+    };
+    version.version = 70016;
+    conn.write_packet(&PacketPayloadType::Version(Cow::Owned(version)))
+        .await?;
+
+    let mut remote_version: Option<Version<'a>> = None;
+    let mut finished = false;
+
+    while !finished {
+        let header = conn.read_header().await?;
+        let mut allocator = conn.prepare_for_read().await;
+        let packet = conn.read_packet(header, &mut allocator).await?;
+        if let Some(payload) = packet.payload {
+            let responses =
+                handle_packet_during_handshaking(payload, &mut finished, &mut remote_version);
+            drop(allocator);
+            if let Some(responses) = responses {
+                for r in &responses {
+                    conn.write_packet(&r).await?;
+                }
+            }
+        }
+    }
+
+    Ok(remote_version.unwrap())
+}
+
+fn handle_packet_during_handshaking<'a, 'c, 'b: 'c>(
+    packet: PacketPayloadType<'c>,
+    success: &mut bool,
+    remote_version: &mut Option<Version<'b>>,
+) -> Option<Vec<PacketPayloadType<'a>>> {
+    match packet {
+        PacketPayloadType::Version(v) => {
+            let n: Version<'b> = v.deep_clone();
+            *remote_version = Some(n)
+        }
+        PacketPayloadType::VerAck(_) => {
+            *success = true;
+            let mut responses = Vec::with_capacity(1);
+            responses.push(PacketPayloadType::VerAck(Cow::Owned(VerAck {})));
+            return Some(responses);
+        }
+        _ => {}
+    }
+    None
+}
+
+async fn connect(peer: &AddressPortNetwork) -> Result<Connection> {
+    let stream = tokio::net::TcpStream::connect(peer.to_string()).await?;
+    let (a, b) = stream.into_split();
+    let bufwriter = BufWriter::new(b);
+    let bufreader = BufReader::new(a);
+
+    Ok(Connection {
+        write_stream: bufwriter,
+        read_stream: bufreader,
+    })
+}
