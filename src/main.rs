3a4105ff01a6b827b81f466d3db916ab331ce6aa
Charterino <charterinod@gmail.com>
2025-06-11 13:23:52 +0300

    chore: on startup resolve some dns seeds and add them to addrman

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 //#![allow(async_fn_in_trait, unused_variables)]
 
-use std::sync::mpsc::{Receiver, channel};
+use std::{
+    net::ToSocketAddrs,
+    sync::{
+        Mutex,
+        mpsc::{Receiver, channel},
+    },
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
-use anyhow::Result;
-use tokio::{fs::File, io::AsyncWriteExt};
+use anyhow::{Result, bail};
+use tokio::{
+    fs::File,
+    io::AsyncWriteExt,
+    select,
+    time::{Instant, sleep_until},
+};
 
 use pprof::protos::Message;
+use types::{addressportnetwork::AddressPortNetwork, network_id::NetworkId};
 
 pub mod addrman;
 pub mod connect;
@@ -14,14 +27,17 @@ pub mod db;
 pub mod packets;
 pub mod types;
 
-fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
-    let (sender, receiver) = channel();
-    ctrlc::set_handler(move || {
-        sender.send(()).unwrap();
-    })?;
-
-    Ok(receiver)
-}
+const DNS_SEEDS: &[&'static str] = &[
+    "seed.bitcoin.sipa.be",
+    "dnsseed.bluematt.me",
+    "dnsseed.bitcoin.dashjr-list-of-p2p-nodes.us",
+    "seed.bitcoin.jonasschnelli.ch",
+    "seed.btc.petertodd.net",
+    "seed.bitcoin.sprovoost.nl",
+    "dnsseed.emzy.de",
+    "seed.bitcoin.wiz.biz",
+    "seed.mainnet.achownodes.xyz",
+];
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -34,6 +50,7 @@ async fn main() -> Result<()> {
         .unwrap();
     db::setup().await;
     addrman::start().await;
+    tokio::spawn(resolve_dns_and_add_to_addrman(DNS_SEEDS));
     let c = tokio::spawn(crawler::crawl_forever());
 
     println!("waiting");
@@ -53,4 +70,76 @@ async fn main() -> Result<()> {
     file.write_all(&content).await.unwrap();
 
     Ok(())
+}
+
+async fn resolve_dns_and_add_to_addrman(peers: &[&str]) {
+    let mut resolved_peers = Vec::new();
+    let mut handles = Vec::with_capacity(peers.len());
+    for peer in peers {
+        handles.push(resolve_dns_with_timeout(peer, Duration::from_secs(1)));
+    }
+    for i in handles.len()..0 {
+        let handle = handles.pop().unwrap();
+        let addy = peers[i - 1];
+        match handle.await {
+            Ok(addys) => resolved_peers.extend_from_slice(&addys),
+            Err(e) => println!("failed to resolve {}: {}", addy, e),
+        }
+    }
+    resolved_peers.sort();
+    resolved_peers.dedup();
+    addrman::peers_seen(
+        resolved_peers,
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+    )
+    .await;
+}
+
+async fn resolve_dns_with_timeout(
+    peer: &str,
+    timeout: Duration,
+) -> Result<Vec<AddressPortNetwork>> {
+    let deadline = Instant::now().checked_add(timeout).unwrap();
+    let cloned_address = peer.to_string();
+    let resolve = tokio::spawn(async move { resolve_dns(cloned_address) });
+    select! {
+        _ = sleep_until(deadline) => {
+            bail!("deadline reached")
+        }
+        resolved = resolve => {
+            resolved.unwrap()
+        }
+    }
+}
+
+fn resolve_dns(peer: String) -> Result<Vec<AddressPortNetwork>> {
+    match (peer, 8333).to_socket_addrs() {
+        Ok(addresses) => Ok(addresses
+            .map(|x| AddressPortNetwork {
+                network_id: if x.is_ipv4() {
+                    NetworkId::IPv4
+                } else {
+                    NetworkId::IPv6
+                },
+                port: x.port(),
+                address: match x.ip().to_canonical() {
+                    std::net::IpAddr::V4(ipv4_addr) => ipv4_addr.octets().into(),
+                    std::net::IpAddr::V6(ipv6_addr) => ipv6_addr.octets().into(),
+                },
+            })
+            .collect()),
+        Err(e) => bail!(e),
+    }
+}
+
+fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
+    let (sender, receiver) = channel();
+    ctrlc::set_handler(move || {
+        sender.send(()).unwrap();
+    })?;
+
+    Ok(receiver)
 }
