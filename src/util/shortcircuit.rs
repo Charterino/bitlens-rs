9b5ad0a36526086c0953186634674686d657b699
Charterino <charterinod@gmail.com>
2025-06-18 21:02:11 +0300

    chore: add some_or_break and ok_or_return

--- src/util/shortcircuit.rs
+++ src/util/shortcircuit.rs
@@ -9,6 +9,16 @@ macro_rules! some_or_return {
 }
 
 #[macro_export]
+macro_rules! some_or_break {
+    ($item:expr) => {
+        match $item {
+            Some(v) => v,
+            None => break,
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! ok_or_continue {
     ($item:expr) => {
         match $item {
@@ -28,5 +38,18 @@ macro_rules! ok_or_break {
     };
 }
 
+#[macro_export]
+macro_rules! ok_or_return {
+    ($item:expr) => {
+        match $item {
+            Ok(v) => v,
+            Err(_) => return,
+        }
+    };
+}
+
+pub use ok_or_break;
 pub use ok_or_continue;
+pub use ok_or_return;
+pub use some_or_break;
 pub use some_or_return;
