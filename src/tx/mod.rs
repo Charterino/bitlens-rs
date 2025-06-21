82e3e3fa39e0caed453d0034aa6235c78cecfcc4
Charterino <charterinod@gmail.com>
2025-06-21 07:30:16 +0300

    chore: empty implementation of analyze_tx for now

--- src/tx/mod.rs
+++ src/tx/mod.rs
@@ -1,8 +1,26 @@
 use crate::{
-    db,
+    db::{self, rocksdb::SerializedTx},
     packets::tx::{Tx, TxOut},
+    util::arena::Arena,
 };
 
-pub fn analyze_tx(tx: &Tx, dependencies: &[&TxOut]) -> db::rocksdb::SerializedTx {
-    todo!()
+pub fn analyze_tx<'arena, 'data>(
+    tx: &'data Tx,
+    dependencies: &'data [&'data TxOut],
+    arena: &'arena Arena,
+) -> db::rocksdb::SerializedTx<'arena> {
+    let analyzed_tx = arena
+        .try_alloc_array_fill_copy(0, 0)
+        .expect("to have allocated analyzed_tx");
+    let tx_outs = arena
+        .try_alloc_array_fill_copy(0, 0)
+        .expect("to have allocated tx_outs");
+    SerializedTx {
+        hash: tx.hash,
+        analyzed_tx: Some(analyzed_tx),
+        tx_outs: Some(tx_outs),
+        fee: 0,
+        value: 0,
+        size: 0,
+    }
 }
