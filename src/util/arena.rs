4aace5107aa2023bafb2b23090cd7f9291f2a57e
Charterino <charterinod@gmail.com>
2025-06-21 13:49:51 +0300

    fix: proper alignment in arena

--- src/util/arena.rs
+++ src/util/arena.rs
@@ -55,25 +55,38 @@ impl Arena {
         count: usize,
         value: T,
     ) -> Result<&mut [T]> {
-        let size = size_of::<T>() * count;
+        let size_t = size_of::<T>();
+        let size = size_t * count;
         let mut currently_consumed = self.consumed.load(Ordering::Relaxed);
         let cap = self.capacity;
+        let mut padding = if size != 0 {
+            size - (self.data as usize + currently_consumed) % size
+        } else {
+            0
+        };
         while currently_consumed + size <= cap {
             match self.consumed.compare_exchange_weak(
                 currently_consumed,
-                currently_consumed + size,
+                currently_consumed + padding + size,
                 Ordering::Relaxed,
                 Ordering::Relaxed,
             ) {
                 Ok(_) => unsafe {
-                    let location = self.data.add(currently_consumed);
+                    let location = self.data.add(padding + currently_consumed);
                     let casted: *mut T = transmute(location);
                     for i in 0..count {
                         ptr::write(casted.add(i), value);
                     }
                     return Ok(slice::from_raw_parts_mut(casted, count));
                 },
-                Err(new_currently_consumed) => currently_consumed = new_currently_consumed,
+                Err(new_currently_consumed) => {
+                    currently_consumed = new_currently_consumed;
+                    padding = if size != 0 {
+                        size - (self.data as usize + currently_consumed) % size
+                    } else {
+                        0
+                    };
+                }
             }
         }
         bail!("arena: out of memory")
@@ -84,20 +97,32 @@ impl Arena {
         let size = size_of::<T>() * value.len();
         let mut currently_consumed = self.consumed.load(Ordering::Relaxed);
         let cap = self.capacity;
+        let mut padding = if size != 0 {
+            size - (self.data as usize + currently_consumed) % size
+        } else {
+            0
+        };
         while currently_consumed + size <= cap {
             match self.consumed.compare_exchange_weak(
                 currently_consumed,
-                currently_consumed + size,
+                currently_consumed + padding + size,
                 Ordering::Relaxed,
                 Ordering::Relaxed,
             ) {
                 Ok(_) => unsafe {
-                    let location = self.data.add(currently_consumed);
+                    let location = self.data.add(padding + currently_consumed);
                     let casted: *mut T = transmute(location);
                     core::ptr::copy_nonoverlapping(value.as_ptr(), casted, value.len());
                     return Ok(slice::from_raw_parts_mut(casted, value.len()));
                 },
-                Err(new_currently_consumed) => currently_consumed = new_currently_consumed,
+                Err(new_currently_consumed) => {
+                    currently_consumed = new_currently_consumed;
+                    padding = if size != 0 {
+                        size - (self.data as usize + currently_consumed) % size
+                    } else {
+                        0
+                    };
+                }
             }
         }
         bail!("arena: out of memory")
