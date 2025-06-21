2d3b420721f0c3704961271e6ea158ceab32f657
Charterino <charterinod@gmail.com>
2025-06-21 08:03:20 +0300

    chore: blocksync WIP

--- src/chainman/blocksync.rs
+++ src/chainman/blocksync.rs
@@ -1,7 +1,8 @@
 use std::{
     borrow::Cow,
+    collections::HashMap,
     num::NonZeroUsize,
-    sync::atomic::Ordering,
+    sync::{Arc, Mutex, RwLock, atomic::Ordering},
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
@@ -12,15 +13,23 @@ use tokio::{
 };
 
 use crate::{
-    addrman, ok_or_break, ok_or_continue, ok_or_return,
+    addrman,
+    db::{self, rocksdb::SerializedTx},
+    ok_or_break, ok_or_continue, ok_or_return,
     packets::{
+        block::Block,
         getdata::GetData,
         invvector::{InventoryVector, InventoryVectorType},
-        packet::{self, PayloadWithAllocator},
+        packet::{self, MAX_PACKET_SIZE, PayloadWithAllocator},
         packetpayload::PacketPayloadType,
+        tx::TxOut,
+        varstr::VarStr,
     },
-    some_or_return, with_deadline,
+    some_or_return, tx,
+    util::arena::Arena,
+    with_deadline,
 };
+use anyhow::{Result, bail};
 
 use super::get_block_hashes_to_download;
 
@@ -40,23 +49,14 @@ pub async fn sync_blocks() {
         packet::MAX_DESERIALIZE_ARENA_COUNT_DURING_BLOCKSYNC,
         Ordering::Relaxed,
     );
-    let (flush_tx, mut flush_rx) = channel(1);
+    let (flush_tx, flush_rx) = channel(1);
     let (master_tx, master_rx) = channel(1);
     // Todo: spawn and kill workers dynamically
     for _ in 0..50 {
         tokio::spawn(run_download_worker(master_tx.clone()));
     }
     tokio::spawn(run_master(master_rx, flush_tx));
-    // TODO: spawn a task that receives blocks and processes them and writes them to disk.
-    // This is just an filler code for now that simply receives blocsk from `flush_rx` and drops them after 2 seconds
-    while let Some(b) = flush_rx.recv().await {
-        tokio::spawn(async move {
-            tokio::time::sleep(Duration::from_secs(2)).await;
-            if b.1 % 100 == 0 {
-                info!("recv block yay"; "block" => b.1);
-            }
-        });
-    }
+    tokio::spawn(receive_and_process_blocks(flush_rx));
     packet::CURRENT_POOL_LIMIT.store(packet::INITIAL_DESERIALIZE_ARENA_COUNT, Ordering::Relaxed);
 }
 
@@ -318,4 +318,189 @@ fn job_to_getdata(hash: [u8; 32]) -> PacketPayloadType<'static> {
             hash: Cow::Owned(hash),
         })]),
     }))
+}
+
+const MAX_BLOCKS_PER_FLUSH: usize = 1024;
+type AnalyzedBlock<'a> = &'a [SerializedTx<'a>];
+struct ChainSyncState<'arena> {
+    current_txouts: HashMap<[u8; 32], Vec<&'arena TxOut<'arena>>>,
+    current_analyzed: Vec<AnalyzedBlock<'arena>>,
+    previous_txouts: HashMap<[u8; 32], Vec<&'arena TxOut<'arena>>>,
+}
+
+async fn receive_and_process_blocks(mut recv: Receiver<(PayloadWithAllocator, u64)>) {
+    let mut current_blocks = Vec::with_capacity(MAX_BLOCKS_PER_FLUSH);
+    let current_state = Arc::new(RwLock::new(ChainSyncState {
+        current_txouts: HashMap::new(),
+        previous_txouts: HashMap::new(),
+        current_analyzed: Vec::with_capacity(MAX_BLOCKS_PER_FLUSH),
+    }));
+    let current_arena: Arena = Arena::new(MAX_BLOCKS_PER_FLUSH * MAX_PACKET_SIZE);
+    // TODO
+    while let Some(b) = recv.recv().await {
+        // Have to instantly insert transactions from this block because they might be needed in the next block
+        b.0.with_block(|block| {
+            current_blocks.push(block.header.hash);
+            let mut w = current_state.write().unwrap();
+            insert_transactions_from_block(block, &current_arena, &mut w.current_txouts)
+                .expect("to have inserted txs from the current block");
+        });
+
+        // local.spawn_local(process_block(b.0, current_state.clone(), &current_arena));
+
+        if current_blocks.len() == MAX_BLOCKS_PER_FLUSH {
+            // flush
+        }
+    }
+}
+
+fn insert_transactions_from_block<'arena, 'block>(
+    block: &Block<'block>,
+    arena: &'arena Arena,
+    txouts: &mut HashMap<[u8; 32], Vec<&'arena TxOut<'arena>>>,
+) -> Result<()> {
+    for tx in block.txs.iter() {
+        let mut cloned = Vec::with_capacity(tx.txouts.len());
+        for txout in tx.txouts.iter() {
+            let cloned_script = arena.try_alloc_array_copy(txout.script.inner.as_ref())?;
+            let txout = match arena.try_alloc(TxOut {
+                value: txout.value,
+                script: Cow::Owned(VarStr {
+                    inner: Cow::Borrowed(cloned_script),
+                }),
+            }) {
+                Ok(p) => p,
+                Err(_) => bail!("oom"),
+            };
+            cloned.push(&*txout);
+        }
+        txouts.insert(tx.hash, cloned);
+    }
+    Ok(())
+}
+
+async fn process_block<'arena>(
+    payload: PayloadWithAllocator,
+    current_state: Arc<RwLock<ChainSyncState<'arena>>>,
+    arena: &'static Arena,
+) {
+    payload
+        .with_block_async(async |block| {
+            // First we load all dependencies we dont have from disk:
+            let deps_from_disk = fetch_missing_dependencies(block, current_state.clone()).await;
+            let mut deps_from_disk_index = 0;
+
+            let r = current_state.read().unwrap();
+            let current_txouts = &r.current_txouts;
+            let previous_txouts = &r.previous_txouts;
+            // Then we assemble dependency arrays
+            // Every dependency might be
+            // - current_txouts: blocks that are still being processed
+            // - previous_txouts: last batch of block that is already processed and is being flushed to disk
+            // - deps_from_disk
+            let analyzed_txs = arena
+                .try_alloc_array_fill_copy(block.txs.len(), SerializedTx::default())
+                .expect("to have allocated analyzed_txs");
+            for tx_idx in 0..block.txs.len() {
+                let tx = &block.txs[tx_idx];
+                // Assemble the dependencies
+                let mut deps = Vec::with_capacity(tx.txins.len());
+                for txin_idx in 0..tx.txins.len() {
+                    let txin = &tx.txins[txin_idx];
+                    if *txin.prevout_hash == [0u8; 32] {
+                        continue;
+                    }
+                    // Is it in current_txouts?..
+                    if let Some(txouts) = current_txouts.get(&*txin.prevout_hash) {
+                        let p = *txouts.get(txin.prevout_index as usize).unwrap();
+                        deps.push(&*p);
+                        continue;
+                    }
+                    // Maybe in previous_txouts?..
+                    if let Some(txouts) = previous_txouts.get(&*txin.prevout_hash) {
+                        let p = *txouts.get(txin.prevout_index as usize).unwrap();
+                        deps.push(&*p);
+                        continue;
+                    }
+                    // Must be in deps_from_disk at `current_deps_index`
+                    let dep: &TxOut<'static> = &deps_from_disk[deps_from_disk_index];
+                    let c: &TxOut<'arena> = dep.covariant();
+                    deps.push(c);
+                    deps_from_disk_index += 1;
+                }
+                // Analyze this tx
+                let analyzed = tx::analyze_tx(tx, &deps, arena);
+                analyzed_txs[tx_idx] = analyzed;
+            }
+            drop(r);
+            // insert analyzed_txs into current_state.
+            let mut w = current_state.write().unwrap();
+            w.current_analyzed.push(analyzed_txs);
+        })
+        .await;
+}
+
+async fn fetch_missing_dependencies(
+    block: &Block<'_>,
+    current_state: Arc<RwLock<ChainSyncState<'_>>>,
+) -> Vec<TxOut<'static>> {
+    let loaded_deps = Arc::new(Mutex::new(Vec::new()));
+    let mut loaded_deps_handles = Vec::new();
+    {
+        let r = current_state.read().unwrap();
+        let mut w = loaded_deps.lock().unwrap();
+        for tx in block.txs.iter() {
+            for txin_idx in 0..tx.txins.len() {
+                let txin = &tx.txins[txin_idx];
+                // Skip empty hashes
+                if *txin.prevout_hash == [0u8; 32] {
+                    continue;
+                }
+                // Skip hashes that we have in current_txouts
+                if r.current_txouts.contains_key(&*txin.prevout_hash) {
+                    continue;
+                }
+                // Skip hashes that we have in previous_txouts
+                if r.previous_txouts.contains_key(&*txin.prevout_hash) {
+                    continue;
+                }
+                w.push(None);
+                loaded_deps_handles.push(tokio::spawn(fetch_dependency_and_insert(
+                    txin.prevout_hash.clone().into_owned(),
+                    txin.prevout_index as usize,
+                    loaded_deps.clone(),
+                    w.len(),
+                )));
+            }
+        }
+    }
+    // Wait for all tasks to finish
+    for h in loaded_deps_handles {
+        h.await.expect("the task to finish successfully");
+    }
+    // Now we know loaded_deps is populated
+    let loaded_deps = Arc::try_unwrap(loaded_deps)
+        .expect("all other arcs to be dropped")
+        .into_inner()
+        .expect("to take ownership of loaded_deps");
+
+    // convert Vec<Option<T>> into Vec<T>, unwrapping all options
+    loaded_deps
+        .into_iter()
+        .map(|option| option.expect("for the option to be Some"))
+        .collect()
+}
+
+async fn fetch_dependency_and_insert(
+    txout_hash: [u8; 32],
+    txout_index: usize,
+    deps: Arc<Mutex<Vec<Option<TxOut<'static>>>>>,
+    deps_index: usize,
+) {
+    let txouts = db::rocksdb::get_transaction_outputs(txout_hash)
+        .await
+        .expect("to have found txouts in the db");
+    let target = txouts[txout_index].clone();
+    let mut w = deps.lock().unwrap();
+    w[deps_index] = Some(target);
 }
