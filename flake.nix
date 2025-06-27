f3d46faf739e0acae8525ac039e01d9a15aa14c5
Charterino <charterinod@gmail.com>
2025-06-27 21:30:17 +0300

    feat: add nix build targets

--- flake.nix
+++ flake.nix
@@ -12,20 +12,74 @@
     }:
     let
       systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
+        {
+          name = "x86_64-linux";
+          cargoHash = "sha256-DrBt2kJQvMyz9kEYCMzI/E/b/JzN1OrOXleFmQKt6vo=";
+        }
+        {
+          name = "aarch64-linux";
+          cargoHash = "sha256-DrBt2kJQvMyz9kEYCMzI/E/b/JzN1OrOXleFmQKt6vo=";
+        }
+        {
+          name = "aarch64-darwin";
+          cargoHash = "sha256-DrBt2kJQvMyz9kEYCMzI/E/b/JzN1OrOXleFmQKt6vo=";
+        }
       ];
+      genericGenAttrs =
+        values: f:
+        builtins.listToAttrs (
+          map (v: {
+            name = v.name;
+            value = f v;
+          }) values
+        );
       forAllSystems =
         f:
-        nixpkgs.lib.genAttrs systems (
-          system:
+        genericGenAttrs systems (
+          { name, cargoHash }:
           f {
-            pkgs = nixpkgs.legacyPackages.${system};
+            pkgs = nixpkgs.legacyPackages.${name};
+            inherit cargoHash;
           }
         );
     in
     {
+      packages = forAllSystems (
+        { pkgs, cargoHash }:
+        let
+          bin = pkgs.rustPlatform.buildRustPackage {
+            pname = "bitlens";
+            name = "bitlens";
+            src = pkgs.lib.cleanSource ./.;
+            inherit cargoHash;
+            preBuild = ''
+              # This fixes building the flake on an aarch64 github runner that has a /homeless-shelter present
+              export HOME=$(pwd)
+              export LIBCLANG_PATH=${pkgs.libclang.lib}/lib
+            '';
+          };
+
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "bitlens";
+            tag = "latest";
+            copyToRoot = [
+              bin
+              pkgs.cacert
+            ];
+            config = {
+              WorkingDir = "/data";
+              Cmd = [
+                "/bin/bitlens-rs"
+              ];
+            };
+          };
+        in
+        {
+          inherit bin dockerImage;
+          default = bin;
+        }
+      );
+
       devShells = forAllSystems (
         { pkgs, ... }:
         {
