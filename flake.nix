21fb83d10fb2799aa424ed8813ec9054d0370eee
Charterino <charterinod@gmail.com>
2025-07-01 09:46:53 +0300

    chore: replace cargoHash with cargoLock in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,46 +12,30 @@
     }:
     let
       systems = [
-        {
-          name = "x86_64-linux";
-          cargoHash = "sha256-DrBt2kJQvMyz9kEYCMzI/E/b/JzN1OrOXleFmQKt6vo=";
-        }
-        {
-          name = "aarch64-linux";
-          cargoHash = "sha256-DrBt2kJQvMyz9kEYCMzI/E/b/JzN1OrOXleFmQKt6vo=";
-        }
-        {
-          name = "aarch64-darwin";
-          cargoHash = "sha256-DrBt2kJQvMyz9kEYCMzI/E/b/JzN1OrOXleFmQKt6vo=";
-        }
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
-      genericGenAttrs =
-        values: f:
-        builtins.listToAttrs (
-          map (v: {
-            name = v.name;
-            value = f v;
-          }) values
-        );
       forAllSystems =
         f:
-        genericGenAttrs systems (
-          { name, cargoHash }:
+        nixpkgs.lib.genAttrs systems (
+          system:
           f {
-            pkgs = nixpkgs.legacyPackages.${name};
-            inherit cargoHash;
+            pkgs = nixpkgs.legacyPackages.${system};
           }
         );
     in
     {
       packages = forAllSystems (
-        { pkgs, cargoHash }:
+        { pkgs }:
         let
           bin = pkgs.rustPlatform.buildRustPackage.override { stdenv = pkgs.clangStdenv; } {
             pname = "bitlens";
             name = "bitlens";
             src = pkgs.lib.cleanSource ./.;
-            inherit cargoHash;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
             env = {
               LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
               TAR_OPTIONS = "--no-same-owner";
