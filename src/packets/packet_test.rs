3f0b829c895b52e2a3b80e464fa8ed73db12b1c9
Charterino <charterinod@gmail.com>
2025-07-19 13:32:21 +0300

    fix: alloc track test failing in release mode

--- src/packets/packet_test.rs
+++ src/packets/packet_test.rs
@@ -34,9 +34,9 @@ impl AllocationTracker for StdoutTracker {
         wrapped_size: usize,
         group_id: AllocationGroupId,
     ) {
-        // Allocations have all the pertinent information upfront, which you may or may not want to store for further
-        // analysis. Notably, deallocations also know how large they are, and what group ID they came from, so you
-        // typically don't have to store much data for correlating deallocations with their original allocation.
+        if group_id.as_usize().get() != 2 {
+            return;
+        }
         println!(
             "allocation -> addr=0x{:0x} object_size={} wrapped_size={} group_id={:?}",
             addr, object_size, wrapped_size, group_id
@@ -52,11 +52,9 @@ impl AllocationTracker for StdoutTracker {
         source_group_id: AllocationGroupId,
         current_group_id: AllocationGroupId,
     ) {
-        // When a deallocation occurs, as mentioned above, you have full access to the address, size of the allocation,
-        // as well as the group ID the allocation was made under _and_ the active allocation group ID.
-        //
-        // This can be useful beyond just the obvious "track how many current bytes are allocated by the group", instead
-        // going further to see the chain of where allocations end up, and so on.
+        if source_group_id.as_usize().get() != 2 {
+            return;
+        }
         println!(
             "deallocation -> addr=0x{:0x} object_size={} wrapped_size={} source_group_id={:?} current_group_id={:?}",
             addr, object_size, wrapped_size, source_group_id, current_group_id
